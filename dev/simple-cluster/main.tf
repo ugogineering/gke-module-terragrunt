da29c91d0e9d1d4b896f9d8663082a887d7a56db
ugogineering <ugogineering@gmail.com>
2024-02-08 11:50:54 +0100

    Added cluster_name variable in variables.tf file. Updated node-pool module to use cluster_name set in cluster module.

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -2,6 +2,7 @@
 module "cluster" {
   source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=create-node-pool-module"
   project_id = var.project_id
+  cluster_name = var.cluster_name
 }
 module "node-pool" {
   source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=create-node-pool-module"
