6c280daa4c317f625f4706a55fbdde92ab2fced6
ugogineering <ugogineering@gmail.com>
2024-02-08 17:44:22 +0100

    Updated cluster parameter in node-pool module.

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -7,5 +7,5 @@ module "cluster" {
 module "node-pool" {
   source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=create-node-pool-module"
   project_id = var.project_id
-  cluster_name = module.cluster.cluster_name 
+  cluster = module.cluster.cluster_name 
 }
