4ba63cdb6f4d31437a877ee19e0eee77e2a58faa
ugogineering <ugogineering@gmail.com>
2024-02-08 10:17:48 +0100

    Added required project_id to module node-pool in main.tf.

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -5,4 +5,5 @@ module "cluster" {
 }
 module "node-pool" {
   source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=create-node-pool-module"
+  project_id = var.project_id
 }
