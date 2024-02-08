53b063d0af9359d867decd2b1f2f3465a02b8761
ugogineering <ugogineering@gmail.com>
2024-02-08 19:03:05 +0100

    Updated module

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -8,4 +8,5 @@ module "node-pool" {
   source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=create-node-pool-module"
   project_id = var.project_id
   cluster = module.cluster.name 
+  depends_on = [module.cluster]
 }
--- dev/simple-cluster/outputs.tf
+++ dev/simple-cluster/outputs.tf
@@ -1,7 +1,7 @@
-output "cluster_name" {
-  value = module.cluster.name 
-  description = "The name of the cluster being created"
-}
-output "node_pool_name" {
-  value = module.node-pool.name
-}
+#output "cluster_name" {
+#  value = module.cluster.name 
+#  description = "The name of the cluster being created"
+#}
+#output "node_pool_name" {
+#  value = module.node-pool.name
+#}
