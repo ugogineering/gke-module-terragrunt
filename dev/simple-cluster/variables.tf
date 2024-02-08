62d60ed7c8ea98673b9fc773cc94d01b1f273c5f
ugogineering <ugogineering@gmail.com>
2024-02-08 10:11:32 +0100

    Added variables.tf file in the simple-cluster directory and updated it with project_id variable. I uncommented the project_id being inputed in terragrunt.hcl file because I think it is redundant for now.

---
+++ dev/simple-cluster/variables.tf
@@ -0,0 +1,10 @@
+variable "zone" {
+  description = "Default Zone"
+  type = string
+  default = "us-east1-b"
+}
+variable "project_id" {
+  description = "Project ID"
+  type = string 
+  default = "" #Input correct project ID value here.
+}
