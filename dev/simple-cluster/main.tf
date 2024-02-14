c1748bed8caa5924bf1f154965a4dcd21b756de3
ugogineering <ugogineering@gmail.com>
2024-02-14 08:17:42 +0100

    Updated terragrunt to read from use-node-pool-module branch.

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -1,12 +1,12 @@
 # Creating infrastructure using Terragrunt and terraform
 module "cluster" {
-  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=main"
-  project_id = var.project_id
-  cluster_name = var.cluster_name
+  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=use-node-pool-module"
+  #project_id = var.project_id
+  name = var.cluster_name
 }
 module "node-pool" {
-  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=main"
-  project_id = var.project_id
-  cluster = module.cluster.name 
+  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=use-node-pool-module"
+  #project_id = var.project_id
+  cluster = module.cluster.id
   depends_on = [module.cluster]
 }
