2cf3a73b18734822c4b7713189ef9ae75fcd6ff0
ugogineering <ugogineering@gmail.com>
2024-02-15 11:18:13 +0100

    Modified simple-cluster to use helm-releases module to install helm_releases resources (helm charts).

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -1,9 +1,11 @@
 # Creating infrastructure using Terragrunt and terraform
-module "cluster" {
-  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=use-node-pool-module"
+module "clusters" {
+  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=ft-helm-charts"
 }
 module "node-pool" {
-  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=use-node-pool-module"
-  #cluster = module.cluster.id
+  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=ft-helm-charts"
   depends_on = [module.cluster]
+}
+module "helm-releases" {
+  source = "git::https://github.com/ugogineering/test-modules.git//helm-releases?ref=ft-helm-charts"
 }
