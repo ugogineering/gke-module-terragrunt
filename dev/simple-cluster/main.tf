fe89ace802edc6d15796f6c923f694e7b0459e37
ugogineering <ugogineering@gmail.com>
2024-02-15 14:42:51 +0100

    Modified simple-cluster main.tf file to point to ft-helm-cert-manager branch.

--- dev/simple-cluster/main.tf
+++ dev/simple-cluster/main.tf
@@ -1,9 +1,9 @@
 # Creating infrastructure using Terragrunt and terraform
 module "clusters" {
-  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=ft-helm-charts"
+  source = "git::https://github.com/ugogineering/test-modules.git//clusters?ref=ft-helm-cert-manager"
 }
 module "node-pool" {
-  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=ft-helm-charts"
+  source = "git::https://github.com/ugogineering/test-modules.git//node-pool?ref=ft-helm-cert-manager"
   depends_on = [module.clusters]
 }
 module "helm-releases" {
